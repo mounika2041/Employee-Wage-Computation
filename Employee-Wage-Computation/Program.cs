ee3ee47d5851d4f7fdc2d1586fe73b2d08fdc32f
duvvamounika <mounikaduvva05@gmail.com>
2022-03-30 15:16:33 +0530

    add EmployeeWageWorkingHrsandDaysUC6 branch and program

--- Employee-Wage-Computation/Program.cs
+++ Employee-Wage-Computation/Program.cs
@@ -17,17 +17,22 @@ namespace Employee_Wage_Computation
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to employee wage computation");
-            //UC5
+            //UC6
             //consants
             int empWage = 0, empHrs = 0, totalEmpWage = 0;
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int NUM_OF_WORKING_DAYS = 20;
             const int EMP_PER_HR = 20;
+            const int MAX_WORKING_HRS = 100;
+            int workingHrs = 0;
+            int day = 1;
             Random random = new Random();
-            for (int day=1;day<=NUM_OF_WORKING_DAYS;day++)
+
+            while (day<= NUM_OF_WORKING_DAYS && workingHrs<=MAX_WORKING_HRS)
             {
                 int empInput = random.Next(0, 3);
+
                 switch(empInput)
                 {
                     case FULL_TIME:
@@ -46,9 +51,17 @@ namespace Employee_Wage_Computation
                 empWage = EMP_PER_HR * empHrs;
                 Console.WriteLine("Daily wage for day{0} is:{1}", day, empWage);
                 totalEmpWage += empWage;
+                workingHrs += empHrs;
+                if(workingHrs>MAX_WORKING_HRS)
+                {
+                    workingHrs -= empHrs;
+                    break;
+                }
+                day++;
             }
-            Console.WriteLine("Total for {0}days is:{1}", NUM_OF_WORKING_DAYS, totalEmpWage);
+            Console.WriteLine("Totalwage for {0} days and working hrs:{1} and wage is:{2}", day-1, workingHrs, totalEmpWage);
             Console.ReadLine();
+            
         }
     }
 }

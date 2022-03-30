44a671976f5f2bf978be1a6f9bd06430434d4f2c
duvvamounika <mounikaduvva05@gmail.com>
2022-03-30 09:53:46 +0530

    add EmployeeAttendanceUC1 branch and program

--- Employee-Wage-Computation/Program.cs
+++ Employee-Wage-Computation/Program.cs
@@ -8,9 +8,24 @@ namespace Employee_Wage_Computation
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to employee wage computation");
+            //UC1
+            int ispresent = 1;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+
+            //selection statement
+            if (ispresent == empInput)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is Absent");
+            Console.ReadLine();
             Console.ReadLine();
         }
     }
